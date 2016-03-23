0b8791ba0bd4c0f73ad6a2c23a0ff3e1322d0564
MichaelTC5525 <michael.cheung@live.ca>
2016-03-22 23:10:36 -0700

    Added files via upload

---
+++ Roll A Ball/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed = 10f;
+	public Text scoreText;
+	public Text winText;
+	private int score;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		score = 0;
+		SetScoreText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 roll = new Vector3 (moveHorizontal, 0f, moveVertical);
+		rb.AddForce (roll * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pickup")) {
+			other.gameObject.SetActive (false);
+			score += 1;
+			SetScoreText ();
+			if (score == 10) {
+				winText.text = "YOU WIN!";
+			}
+		}
+	}
+
+	void SetScoreText() {
+		scoreText.text = "SCORE: " + score.ToString ();
+	}
+}
---
+++ Roll A Ball/Assets/Scripts/TimeCount.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeCount : MonoBehaviour {
+
+	void Start() {
+		StartCoroutine(Example());
+	}
+
+	IEnumerator Example() {
+		print(Time.time);
+		yield return new WaitForSeconds(5);
+		print(Time.time);
+	}
+}
